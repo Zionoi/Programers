2ca47148a865eee3cb2e735f9b98733457087d34
Zionoi <163436710+Zionoi@users.noreply.github.com>
2024-06-20 20:57:53 +0900

    [level 0] Title: 배열 만들기 2, Time: 1.60 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = l ; i<=r; i++){
+            if(f(i)){
+                list.add(i);
+            }
+        
+        }
+        if(list.isEmpty())
+            list.add(-1);
+        int[] answer = new int[list.size()];
+        for(int i =0 ; i<answer.length;i++){
+        answer[i]=list.get(i); 
+        }
+        
+        return answer;
+    }
+    public static boolean f(int i){
+    if(i==0) return true;
+    
+    while(i>0){
+        int five = i%10;
+        if(five != 5 && five != 0){
+            return false;}
+        
+        i /= 10;
+    }
+    return true;
+}
+}
+
+
+
+
