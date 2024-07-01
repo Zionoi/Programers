b226f95e8789e1a160928ff7e3448352e1050a3c
Zionoi <163436710+Zionoi@users.noreply.github.com>
2024-07-01 11:09:49 +0900

    [level 0] Title: 글자 지우기, Time: 0.53 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        Arrays.sort(indices);
+        StringBuilder sb = new StringBuilder(my_string);
+        for(int i=indices.length-1;i>=0;i--)
+            sb.deleteCharAt(indices[i]);
+        
+        return sb.toString();
+    }
+}
