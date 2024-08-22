08a2986c0899c28a0ecf6d52b522c9beca15828d
Zionoi <163436710+Zionoi@users.noreply.github.com>
2024-08-22 09:41:57 +0900

    [level 0] Title: 제곱수 판별하기, Time: 1.11 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(Math.sqrt(n)%1 == 0){
+            System.out.println(Math.sqrt(n)%1);
+            return 1;
+        }else{
+            System.out.println(Math.sqrt(n)%1);
+            return 2;
+        }
+    }
+}
